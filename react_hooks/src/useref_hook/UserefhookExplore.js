e4de83517e20d36016449d5954b2158558a3e2ae
BrownBoy @ Rafi <rafidmr0438@gmail.com>
2023-01-07 07:15:05 +0530

    explored useRef with useState

--- react_hooks/src/useref_hook/UserefhookExplore.js
+++ react_hooks/src/useref_hook/UserefhookExplore.js
@@ -1,9 +1,11 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const UserefhookExplore = () => {
+  const [todos, setTodos] = useState(['RAFI']);
   const inputRef = useRef(null);
 
   const onClickHandler = () => {
+    setTodos([...todos , inputRef.current.value]);
     inputRef.current.value = "";
     inputRef.current.focus();
   };
@@ -13,7 +15,12 @@ const UserefhookExplore = () => {
       <div>
         <h4>useRef Explore.</h4>
         <input placeholder="Type here..." ref={inputRef}></input>
-        <button onClick={onClickHandler}>Clear</button>
+        <button onClick={onClickHandler}>Add Todo</button>
+
+        <br />
+        <br />
+
+        {todos.map(todo => <p key={todo}>{todo}</p>)}
       </div>
     </>
   );
