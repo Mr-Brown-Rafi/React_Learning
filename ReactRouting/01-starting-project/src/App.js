73d2b41cb57324d55e077127b07833be35e20993
BrownBoy @ Rafi <rafidmr0438@gmail.com>
2023-02-18 11:45:31 +0530

    Defining Routes and activating them

--- ReactRouting/01-starting-project/src/App.js
+++ ReactRouting/01-starting-project/src/App.js
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
+const route = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Product /> },
+]);
+
 function App() {
-  return <div>Test</div>;
+  return <RouterProvider router={route}/>
 }
 
 export default App;
