e8ab673134acd337bf4c92baf861858af8c360a6
Brown Boy Rafi <61969034+Mr-Brown-Rafi@users.noreply.github.com>
2023-02-21 15:30:32 +0000

    Navigation between to pages with Link Component

--- ReactRouting/01-starting-project/src/pages/Home.js
+++ ReactRouting/01-starting-project/src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
+    <>
     <div>Welcome to Home page.</div>
+    <Link to={'/products'}>Products Page.</Link>
+    </>
   )
 }
 
--- ReactRouting/01-starting-project/src/pages/Product.js
+++ ReactRouting/01-starting-project/src/pages/Product.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Product() {
   return (
+    <>
     <div>This is Products page.</div>
+    <Link to={'/'}>HOme Page.</Link>
+    </>
   )
 }
 
