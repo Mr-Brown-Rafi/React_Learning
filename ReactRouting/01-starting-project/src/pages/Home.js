1818b525ddfca2685368c09d43d0b3c986b51429
Brown Boy Rafi <61969034+Mr-Brown-Rafi@users.noreply.github.com>
2023-02-22 09:42:02 +0000

    Link element in react routig

--- ReactRouting/01-starting-project/src/pages/Home.js
+++ ReactRouting/01-starting-project/src/pages/Home.js
@@ -5,6 +5,9 @@ function Home() {
   return (
     <>
     <div>Welcome to Home page.</div>
+    
+    <a href="/products">Products Page</a><br/>
+
     <Link to={'/products'}>Products Page.</Link>
     </>
   )
